ecc3afb92c6b499b2adb55d3bf7940c1721fd755
Bowtiedwave <119373192+0x3at@users.noreply.github.com>
2024-10-08 06:20:27 -0500

    da

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,17 @@
-from .app import create_app
+from os import getenv
+
+from flask import Flask
+from dotenv import load_dotenv, find_dotenv
+
+from app.extensions import blueprint
+
+load_dotenv(find_dotenv())
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+    app.config['DEBUG'] = "False"
+    blueprint.init_app(app)
+
+    return app
